19930833d2fa83e07bd41b8f9e83904ed49bcb9e
Derlys Dominguez <derlys275@gmail.com>
2022-02-24 15:16:24 -0500

    add: create layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './ui/layout/layout.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      // Here we will add our application pages
+    ],
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
