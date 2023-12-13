b3adeb24fa230d1d08408949749a8771aaf71c31
Derlys Dominguez <derlys275@gmail.com>
2023-12-13 16:20:12 +0000

    feature: responsive

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -3,35 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  links : Array<any> = [
-    {
-      name: "Inicio",
-      link: "/"
-    },
-    {
-      name: "Cursos",
-      link: "/course-logic"
-    },
-    {
-      name: "Blog",
-      link: "/"
-    }
-  ];
-  showMenu = false;
-  toggleNavbar(){
-    this.showMenu = !this.showMenu;
+  open = false;
+  toggle() {
+    this.open = !this.open;
   }
-
-
-  constructor() {
-
-  }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
